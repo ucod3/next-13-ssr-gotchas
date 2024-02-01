c2d3087e0c4072ceeb59291ac7a009fa251cc5c1
Usman Butt <usman.butt@outlook.com>
2024-02-01 16:54:08 -0500

    Refactor Counter component to handle server-side rendering

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -3,20 +3,24 @@ import React from 'react';
 
 function Counter() {
   const [count, setCount] = React.useState(() => {
-    return Number(
-      window.localStorage.getItem('saved-count') || 0
-    );
+    // Check if window is defined (i.e., we're in a browser context)
+    if (typeof window !== 'undefined') {
+      return Number(window.localStorage.getItem('saved-count') || 0);
+    } else {
+      // If we're not in a browser context (e.g., server-side rendering), default to 0
+      return 0;
+    }
   });
 
   React.useEffect(() => {
-    window.localStorage.setItem('saved-count', count);
-  }, [count]);
+    // Check if window is defined (i.e., we're in a browser context)
+    if (typeof window !== 'undefined') {
+      setCount(Number(window.localStorage.getItem('saved-count') || 0));
+    }
+  }, []);
 
   return (
-    <button
-      className="count-btn"
-      onClick={() => setCount(count + 1)}
-    >
+    <button className='count-btn' onClick={() => setCount(count + 1)}>
       Count: {count}
     </button>
   );
